5c5039711170e5834db54b0e750e2c9b23fb11c1
Alexandr Ustiantcev <a.ustiantcev@gmail.com>
2022-08-09 00:44:14 +0300

    feat: Добавление интерфейса TaskManager

---
+++ src/manager/TaskManager.java
@@ -0,0 +1,53 @@
+package manager;
+
+import common.Epic;
+import common.SubTask;
+import common.Task;
+
+
+import java.util.List;
+
+public interface TaskManager {
+
+    List<Task> getTaskList();
+
+    List<Epic> getEpicList();
+
+    List<SubTask> getSubTaskList();
+
+    void removeAllTask();
+
+    void removeAllEpic();
+
+    void removeAllSubTask();
+
+    Task getTask(int id);
+
+    Epic getEpic(int id);
+
+    SubTask getSubTask(int id);
+
+    int createTask(Task task);
+
+    int createEpic(Epic epic);
+
+    int createSubTask(Epic epic, SubTask subTask);
+
+    void updateTask(Task task);
+
+    void updateEpic(Epic epic);
+
+    void updateSubTask(SubTask subTask);
+
+    void removeTask(int id);
+
+    void removeEpic(int id);
+
+    void removeSubTask(int id);
+
+    List<SubTask> getAllSubTaskOfEpic(Epic epic);
+
+    List<Task> getHistory();
+}
+
+
