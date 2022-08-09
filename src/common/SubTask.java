8e23d58b5777eff340cc977076e1a1c9100d4016
Alexandr Ustiantcev <a.ustiantcev@gmail.com>
2022-08-10 01:11:20 +0300

    fix: Убран параметр Status из конструктора SubTask, теперь он инициализируется в родительском классе Task

--- src/common/SubTask.java
+++ src/common/SubTask.java
@@ -3,8 +3,8 @@ package common;
 public class SubTask extends Task {
     protected int epicId;
 
-    public SubTask(String name, String description, Status status) {
-        super(name, description, status);
+    public SubTask(String name, String description) {
+        super(name, description);
     }
 
     public int getEpicId() {
