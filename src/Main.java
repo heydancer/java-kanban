ad461c2c55b1e7f6be206372b1e08d3a8df28170
Alexandr Ustiantcev <a.ustiantcev@gmail.com>
2022-08-10 01:20:12 +0300

    fix: в методе main изменены тесты

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import common.*;
-import manager.InMemoryTaskManager;
 import manager.Managers;
 import manager.TaskManager;
 
@@ -9,63 +8,74 @@ public class Main {
 
         TaskManager taskManager = Managers.getDefault();
 
-        Task task1 = new Task("Java", "Сделать домашнее задание", Status.NEW);
-        Task task2 = new Task("Английский", "Поехать на занятие", Status.NEW);
-        Task task3 = new Task("Немецкий", "Поехать на занятие", Status.NEW);
-        Task task4 = new Task("Китайский", "Поехать на занятие", Status.NEW);
-        Task task5 = new Task("Французский", "Поехать на занятие", Status.NEW);
-        Task task6 = new Task("Японский", "Поехать на занятие", Status.NEW);
+        Task task1 = new Task("Java", "Сделать домашнее задание");
+        Task task2 = new Task("Английский", "Поехать на занятие");
+        Task task3 = new Task("Немецкий", "Поехать на занятие");
+        Task task4 = new Task("Китайский", "Поехать на занятие");
+        Task task5 = new Task("Французский", "Поехать на занятие");
+        Task task6 = new Task("Японский", "Поехать на занятие");
 
         Epic epic1 = new Epic("Переезд", "Покупка новой квартиры");
-        SubTask subTask1 = new SubTask("Собрать вещи", "Сложить все вещи по коробкам", Status.NEW);
-        SubTask subTask2 = new SubTask("Собрать игрушки собаки", "Найти все игрушки", Status.NEW);
+        SubTask subTask1 = new SubTask("Собрать вещи", "Сложить все вещи по коробкам");
+        SubTask subTask2 = new SubTask("Собрать игрушки собаки", "Найти все игрушки");
 
         Epic epic2 = new Epic("Магазин", "Поход в магазин");
-        SubTask subTask3 = new SubTask("Купить крупу", "Найти рис", Status.NEW);
+        SubTask subTask3 = new SubTask("Купить крупу", "Найти рис");
 
         //Создаем задачи
         taskManager.createTask(task1);
         taskManager.createTask(task2);
-
-        taskManager.createEpic(epic1);
-        taskManager.createSubTask(epic1, subTask1);
-        taskManager.createSubTask(epic1, subTask2);
-
-        taskManager.createEpic(epic2);
-        taskManager.createSubTask(epic2, subTask3);
-
         taskManager.createTask(task3);
         taskManager.createTask(task4);
         taskManager.createTask(task5);
         taskManager.createTask(task6);
 
-        //Смотрим список всех созданных задач
-        System.out.println(taskManager.getTaskList());
-        System.out.println(taskManager.getEpicList());
-        System.out.println(taskManager.getSubTaskList() + "\n");
+        taskManager.createEpic(epic1);
+        taskManager.createEpic(epic2);
 
-        //Поменяем статус СабТаски и посомтрим изменения в истории
+        taskManager.createSubTask(epic1, subTask1);
+        taskManager.createSubTask(epic1, subTask2);
+        taskManager.createSubTask(epic2, subTask3);
+
+        System.out.println("Создание Task: ");
+        for (Task task : taskManager.getTaskList()) {
+            System.out.println(task);
+        }
+        System.out.println();
+
+        System.out.println("Создание Epic: ");
+        for (Epic epic : taskManager.getEpicList()) {
+            System.out.println(epic);
+        }
+        System.out.println();
+
+        System.out.println("Создание SubTask: ");
+        for (SubTask subTask : taskManager.getSubTaskList()) {
+            System.out.println(subTask);
+        }
+        System.out.println();
+
+        //Меняем статус одной из созданой SubTask
         subTask1.setStatus(Status.DONE);
+        //Обновляем SubTask
         taskManager.updateSubTask(subTask1);
 
         //Обращаемся ко всем задачам
         taskManager.getTask(1);
         taskManager.getTask(2);
+        taskManager.getTask(3);
+        taskManager.getTask(4);
+        taskManager.getTask(5);
+        taskManager.getTask(6);
 
-        taskManager.getEpic(3);
+        taskManager.getEpic(7);
+        taskManager.getEpic(8);
 
-        taskManager.getSubTask(4);
-        taskManager.getSubTask(5);
+        taskManager.getSubTask(9);
+        taskManager.getSubTask(10);
+        taskManager.getSubTask(11);
 
-        taskManager.getEpic(6);
-        taskManager.getSubTask(7);
-
-        taskManager.getTask(8);
-        taskManager.getTask(9);
-        taskManager.getTask(10);
-        taskManager.getTask(11);
-
-        //Выводим историю просмотров задач
+        System.out.println("История просмотров: ");
         for (Task task : taskManager.getHistory()) {
             System.out.println(task);
         }
