953754431fce42a9ca5f8f7494c3b306d1109371
Alexandr Ustiantcev <a.ustiantcev@gmail.com>
2022-09-18 18:42:02 +0300

    feat: добавлены статик методы historyToString и historyFromString

--- src/manager/history/HistoryManager.java
+++ src/manager/history/HistoryManager.java
@@ -2,6 +2,7 @@ package manager.history;
 
 import common.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface HistoryManager {
@@ -11,4 +12,25 @@ public interface HistoryManager {
     void remove(int id);
 
     List<Task> getHistory();
+
+    static String historyToString(HistoryManager manager) {
+        StringBuilder historyIds = new StringBuilder();
+
+        for (Task task : manager.getHistory()) {
+            historyIds.append(task.getId()).append(',');
+        }
+
+        return historyIds.toString();
+    }
+
+    static List<Integer> historyFromString(String value) {
+        List<Integer> historyIds = new ArrayList<>();
+        String[] taskArray = value.split(",");
+
+        for (String element : taskArray) {
+            historyIds.add(Integer.valueOf(element));
+        }
+
+        return historyIds;
+    }
 }
