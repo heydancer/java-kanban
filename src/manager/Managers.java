a98446abc7834da5b8f8c1bbdc85eca6aed02ad8
Alexandr Ustiantcev <a.ustiantcev@gmail.com>
2022-09-18 18:53:51 +0300

    feat: добавлен новый метод getDefaultFileManager

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -2,6 +2,7 @@ package manager;
 
 import manager.history.HistoryManager;
 import manager.history.InMemoryHistoryManager;
+import manager.task.FileBackedTasksManager;
 import manager.task.InMemoryTaskManager;
 import manager.task.TaskManager;
 
@@ -13,5 +14,9 @@ public class Managers {
 
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
+    }
+
+    public static TaskManager getDefaultFileManager() {
+        return new FileBackedTasksManager();
     }
 }
